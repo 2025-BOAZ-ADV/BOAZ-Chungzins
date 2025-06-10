63ea323fb09070f6a727025dda4db0ab89ceb856
심현석 <vanillahub12@gmail.com>
2025-06-10 11:38:47 +0000

    Feat: Fixed minor issues

--- trainers/pretrain.py
+++ trainers/pretrain.py
@@ -56,7 +56,11 @@ class PretrainTrainer:
         progress_bar = tqdm(self.train_loader, desc=f'Epoch {epoch}')
         for batch_idx, (mel, _, _) in enumerate(progress_bar):
             # Augmentation 생성 인스턴스 정의
-            augmenter = create_augmenter(self.config, self.augmentations)
+            augmenter = create_augmenter(
+                self.config.target_sr,
+                self.config.target_sec,
+                self.augmentations
+            )
 
             # 두 개의 augmentation 적용
             aug1, aug2 = augmenter.generate_views(mel)[0], augmenter.generate_views(mel)[1]
